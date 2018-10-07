130c98cf4c11d1737ddc74db923be25534a87ccc
System Administrator <root@Chis-MacBook-Pro.local>
2018-10-07 16:36:25 +0700

    test

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import store from './src/store';
+import Main from './src/Main';
+import {Provider} from 'react-redux';
+export default class App extends React.Component {
+  render() {
+    return (
+        <Provider store={store}>
+          <View style = {styles.container}>
+            <Main/>
+          </View>
+        </Provider>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
